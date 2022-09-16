cddd248ac0cf6046dd78458b0c495bf8c72411e1
Dawid Zieleńczuk <x90xdawid@gmail.com>
2022-09-17 01:16:42 +0200

    Add bloc to loginpage

--- lib/app/features/login/login_page.dart
+++ lib/app/features/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:disney_plus/app/features/login/cubit/login_cubit.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
